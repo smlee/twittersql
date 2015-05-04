a3bfe1c6b97decfd494e225e89edde0f26d80419
smlee <sangmlee23@gmail.com>
2015-05-04 18:43:28 -0400

    Cleaned up

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,11 @@
-var express = require('express');
-var router = express.Router();
-// could use one line instead: var router = require('express').Router();
-
-var bodyParser = require('body-parser');
-var models = require('../models/');
+var express = require('express'), router = express.Router(), bodyParser = require('body-parser'), models = require('../models/');
 
 router.use(bodyParser.urlencoded({extended:false}));
 router.use(bodyParser.json());
 
 var User = models.User, Tweet = models.Tweet;
-
-//var newList = new fql(tweetBank.list);
 module.exports = function (io) {
-    // ...
-    // route definitions, etc.
-    // ...
-    //console.log(models.Tweet);
+
     router.get('/users/:name', function(req, res) {
         var name = req.params.name;
         User.find( {where: { name: name } }).then(function(user){
@@ -23,8 +13,6 @@ module.exports = function (io) {
                 res.render( 'index', { title: 'Twitter.js - Posts by '+name, tweets: list, showForm:true, name:name } );
             })
         })
-
-
     });
 
     router.get('/', function (req, res) {
@@ -39,20 +27,10 @@ module.exports = function (io) {
     router.post('/submit', function(req, res) {
         var name = req.body.name;
         var text = req.body.text;
-        User.find({where: { name: name } }).then(function(result){
-            if(result){
-                Tweet.create({tweet:text, UserId: result.dataValues.id}).then(function(){
+        User.findOrCreate({where: { name: name } }).spread(function(user, result){
+                Tweet.create({tweet:text, UserId: user.id}).then(function(){
                     res.redirect('/');
                 });
-            } else {
-                User.create({name:name, pictureUrl: null}).then(function(user){
-                    Tweet.create({name: user.dataValues.name, UserId: user.dataValues.id}).then(function(){
-                        res.redirect('/');
-                    })
-
-                });
-                //console.log("new user.")
-            }
         });
         io.sockets.emit('new_tweet', { name: name, text: text });
     });
